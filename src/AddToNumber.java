541cdd8b1a9b8d853495e3c909088f0c18a4cb49
Phạm Quốc Trường <90681483+Pquoctruong@users.noreply.github.com>
2023-06-18 22:59:21 +0700

    First Program

---
+++ src/AddToNumber.java
@@ -0,0 +1,29 @@
+public class AddToNumber {
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public static void main(String[] args) {
+
+
+    }
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode val = new ListNode();
+
+        return  val;
+    }
+}
